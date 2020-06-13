e956da7d0c37d2ce0cfe1045391625f18337f53d
leonardoSanBenitez <lsBenitezPereira@gmail.com>
2020-06-13 01:15:22 -0300

    two window layout

--- mobile_app/app.py
+++ mobile_app/app.py
@@ -5,19 +5,45 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+'''
 class MyGrid(Widget):
-    pass
 
 
 class MyApp(App):  # <- Main Class
     def build(self):
-        return MyGrid()
-
-    def pressed(self):
-        print('fuck the police')
-
-
+        grid = MyGrid()
+        grid.userName = "Leonardo"
+        return grid
 
 if __name__ == "__main__":
     MyApp().run()
+'''
+
+class MainWindow(Screen):
+    userName = 'Leonardo'
+    def pressed(self):
+        print(self.userName, '- fuck the police')
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("design.kv")
+
+
+class MyMainApp(App):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
