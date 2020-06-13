29feaf5c7c67d62ee0a2d63e5d90da8dedf3b35c
btluu <32603857+btluu@users.noreply.github.com>
2020-06-13 07:37:29 -0400

    Add files via upload

---
+++ Screens/screens/about.py
@@ -0,0 +1,24 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+Builder.load_string("""
+<AboutScreen>
+    name: 'about'
+    ScrollView:
+        id: scroll
+        do_scroll_x: False
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint_y: None
+            height: dp(800)
+            padding: dp(15)
+            spacing: dp(15)
+            MDLabel:
+                font_style: 'Body1'
+                theme_text_color: 'Secondary'
+                text: ut.get_data('texts')['about']
+""")
+
+
+class AboutScreen(Screen):
+    pass
